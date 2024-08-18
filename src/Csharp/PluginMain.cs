46e3d564fc9bfe97b7213bb6f9f899cf1587b853
YoumiHa <140812890+YoumiHa@users.noreply.github.com>
2024-08-18 22:45:36 +0800

    First commit

---
+++ src/Csharp/PluginMain.cs
@@ -0,0 +1,13 @@
+﻿global using System.Runtime.InteropServices;
+namespace ATRIEssentials
+{
+    public static class PluginMain
+    {
+        [DllExport]
+        
+        public static void load()
+        {
+            Console.WriteLine("LoadSuccess");
+        }
+    }
+}
